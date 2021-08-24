307340930afa25f7de06544410d67a7423b191dd
lgtm <1gtm@users.noreply.github.com>
2021-08-24 12:59:32 -0700

    Generate code for provider@v1.23.1-0.20210629201646-266cb440f82b gen@87d82c5 (#31)
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- controllers/containerregistry/docker_credentials_controller.go
+++ controllers/containerregistry/docker_credentials_controller.go
@@ -20,8 +20,8 @@ package containerregistry
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/go-errors/errors"
 	"github.com/go-logr/logr"
 	tfschema "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	auditlib "go.bytebuilders.dev/audit/lib"
@@ -118,7 +118,7 @@ func (r *DockerCredentialsReconciler) SensitiveSecretWatch(ctx context.Context) 
 
 		sensSec, ok := o.(*v1.Secret)
 		if !ok {
-			log.Error(errors.Errorf("expected a Secret but go a %T", o), "failed to get secret for DockerCredentials")
+			log.Error(fmt.Errorf("expected a Secret but go a %T", o), "failed to get secret for DockerCredentials")
 			return nil
 		}
 
--- controllers/database/connection_pool_controller.go
+++ controllers/database/connection_pool_controller.go
@@ -20,8 +20,8 @@ package database
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/go-errors/errors"
 	"github.com/go-logr/logr"
 	tfschema "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	auditlib "go.bytebuilders.dev/audit/lib"
@@ -118,7 +118,7 @@ func (r *ConnectionPoolReconciler) SensitiveSecretWatch(ctx context.Context) han
 
 		sensSec, ok := o.(*v1.Secret)
 		if !ok {
-			log.Error(errors.Errorf("expected a Secret but go a %T", o), "failed to get secret for ConnectionPool")
+			log.Error(fmt.Errorf("expected a Secret but go a %T", o), "failed to get secret for ConnectionPool")
 			return nil
 		}
 
